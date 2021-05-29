0b012f4693442d9282629cf030dfb57314001da2
leventerguder <erguder.levent@gmail.com>
2021-05-29 15:52:12 +0300

    spring-security-simple-example

---
+++ spring-security-simple-example/spring-security-simple-example/src/test/java/com/injavawetrust/SpringSecuritySimpleExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.injavawetrust;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecuritySimpleExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
